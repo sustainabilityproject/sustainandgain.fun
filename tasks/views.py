996aa7820eefdf72ca2e3d9d574f5ccd82690541
hj236 <94056440+hj236@users.noreply.github.com>
2023-02-15 15:39:45 +0000

    Updated task view
    Added task view with some basic (placeholder) templating and CSS
    
    Co-Authored-By: nabe2002 <99815225+nabe2002@users.noreply.github.com>

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Tasks index")
+    tasks_list = Task.objects.all()
+    context = {'tasks_list': tasks_list}
+    return render(request, 'tasks/index_bootstrap.html', context)
+
+
+def my_tasks(request):
+    return HttpResponse("Here are the tasks you currently have active!")
