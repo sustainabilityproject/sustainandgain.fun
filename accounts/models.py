7c5db65055697524c036a66e23ab93144dc53f5d
James Calnan <james.calnan@outlook.com>
2023-02-15 22:34:54 +0000

    added friends pages
    basic functionality:
    add friends
    remove friends
    send requests
    delete request
    
    things like sending the same request twice cause errors

--- accounts/models.py
+++ accounts/models.py
@@ -11,6 +11,36 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
 
+class Friend(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends')
+    friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends_with')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'friend')
+
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend_requests_sent')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend_requests_received')
+    message = models.CharField(max_length=255, blank=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('declined', 'Declined'),
+    )
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f'{self.from_user} -> {self.to_user}: {self.status}'
+
+    class Meta:
+        unique_together = ('from_user', 'to_user')
+
+
 class User(AbstractUser):
     pass
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,12 @@
 from accounts.forms import UserCreationForm
-from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy, reverse
 from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import Friend, FriendRequest
 
 
 class RegisterView(generic.CreateView):
@@ -11,3 +16,90 @@ class RegisterView(generic.CreateView):
 
 def profile(request):
     return render(request, 'account/profile.html')
+
+# render the friends list page with the incoming and outgoing friend requests
+@login_required
+def friends_list(request):
+    friends = request.user.friends.all()
+    users = User.objects.exclude(id=request.user.id)
+
+    incoming_requests = FriendRequest.objects.filter(to_user=request.user)
+    outgoing_requests = FriendRequest.objects.filter(from_user=request.user)
+
+    return render(request, 'account/friends_list.html', {'friends': friends,
+                                                         'users': users,
+                                                         'incoming_requests': incoming_requests,
+                                                         'outgoing_requests': outgoing_requests})
+
+# remove friend from both users friends lists
+@login_required
+def remove_friend(request, username):
+    friend = get_object_or_404(Friend, friend__username=username, user=request.user)
+    friend2 = get_object_or_404(Friend, user__username=username, friend=request.user)
+    friend.delete()
+    friend2.delete()
+    return redirect('friends_list')
+
+# cancel the friend request removes from both users pages
+@login_required
+def cancel_friend_request(request, friend_request_id):
+    friend_request = get_object_or_404(FriendRequest, id=friend_request_id)
+
+    if friend_request.from_user == request.user:
+        friend_request.delete()
+        messages.success(request, 'Friend request canceled.')
+    else:
+        messages.error(request, 'You do not have permission to cancel this friend request.')
+
+    return redirect('friends_list')
+
+# adding friends works by creating a new friend request object which is then displayed 
+# on the outgoing and incoming friends pages
+@login_required
+def add_friend(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        friend = get_object_or_404(User, username=username)
+
+        if not request.user.friends.filter(friend=friend).exists():
+            FriendRequest.objects.create(from_user=request.user, to_user=friend)
+            messages.success(request, f"Friend request sent to {friend.username}")
+        else:
+            messages.error(request, f"{friend.username} is already your friend.")
+
+    return HttpResponseRedirect(reverse('friends_list'))
+
+
+# accepting the friend request works by taking the data in the friendrequest object and then creating a new friends object
+@login_required
+def accept_friend_request(request, request_id):
+    # Get the friend request
+    friend_request = get_object_or_404(FriendRequest, id=request_id)
+    
+    # Check if the request is addressed to the logged in user
+    if friend_request.to_user != request.user:
+        raise Http404
+    
+    # Add the sender to the user's friends list
+    Friend.objects.create(user=request.user, friend=friend_request.from_user)
+    Friend.objects.create(user=friend_request.from_user, friend=request.user)
+    
+    # Delete the friend request
+    friend_request.delete()
+    
+    return redirect('friends_list')
+
+# this method works by just deleting the friendrequest object
+@login_required
+def decline_friend_request(request, request_id):
+    # Get the friend request
+    friend_request = get_object_or_404(FriendRequest, id=request_id)
+    
+    # Check if the request is addressed to the logged in user
+    if friend_request.to_user != request.user:
+        raise Http404
+    
+    # Delete the friend request
+    friend_request.delete()
+    
+    return redirect('friends_list')
