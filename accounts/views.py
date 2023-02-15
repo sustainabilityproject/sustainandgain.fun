0998e499f002491fdbde82cc72bbeedb4b3cd60e
NicholasJAlexander <82706466+NicholasJAlexander@users.noreply.github.com>
2023-02-15 17:57:21 +0000

    created a very simple profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render
 
 
 class RegisterView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/register.html"
+
+def profile(request):
+    return render(request, 'account/profile.html')
