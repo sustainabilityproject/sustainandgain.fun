f46e4134b8920813da085c59997a452635dd4fe8
James Calnan <james.calnan@outlook.com>
2023-02-15 22:34:54 +0000

    added friends pages
    basic functionality:
    add friends
    remove friends
    send requests
    delete request
    
    things like sending the same request twice cause errors

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django. contrib. auth. models import User
+from django.conf import settings
+from django.utils import timezone
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +9,35 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+class Friend(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends')
+    friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends_with')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'friend')
+        
+
+
+STATUS_CHOICES = (
+    ('pending', 'Pending'),
+    ('accepted', 'Accepted'),
+    ('declined', 'Declined'),
+)
+
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend_requests_sent')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend_requests_received')
+    message = models.CharField(max_length=255, blank=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    def __str__(self):
+            return f'{self.from_user} -> {self.to_user}: {self.status}'
+
+       
+    class Meta:
+        unique_together = ('from_user', 'to_user')
