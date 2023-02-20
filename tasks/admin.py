478b180a6f07df7a59bba8943d05decd43d33038
hj236 <94056440+hj236@users.noreply.github.com>
2023-02-20 20:47:37 +0000

    Updated task views
    Task index and user task views are both class rather than function based now.
    The user tasks view (mytasks) has been set up and displays active tasks for the current user.

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
+
 from .models import Task, TaskCategory, TaskInstance
 
 admin.site.register(Task)
 admin.site.register(TaskCategory)
 admin.site.register(TaskInstance)
-
-
