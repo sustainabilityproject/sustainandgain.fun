763758ef54b1419e513528874d67c8fb3ecf1cd5
Charles MacDonald-Smith <ch@rlie.xyz>
2023-02-23 19:45:26 +0000

    Redesign my tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from tasks.views import IndexView, MyTasksView, accept_task
 
-from . import views
-
+from tasks.views import IndexView, MyTasksView, accept_task, CompleteTaskView, TakePhotoView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='tasks_index'),
     path('mytasks/', MyTasksView.as_view(), name='my_tasks'),
     path('<int:task_id>/accept', accept_task, name='accept_task'),
-    path('take-photo/', views.TakePhotoView.as_view(), name='take_photo')
+    path('<int:pk>/complete', CompleteTaskView.as_view(), name='complete_task'),
+    path('take-photo/', TakePhotoView.as_view(), name='take_photo')
 ]
