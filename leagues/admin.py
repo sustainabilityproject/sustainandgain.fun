d1e4dc02bc0ce206da1a85795c1860be719513f7
Charles MacDonald-Smith <ch@rlie.xyz>
2023-02-23 12:46:30 +0000

    Improve League list

--- leagues/admin.py
+++ leagues/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+
 from leagues.models import League
 
-admin.site.register(League)
+
+class LeagueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description',)
+    filter_horizontal = ('members',)
+
+
+admin.site.register(League, LeagueAdmin)
--- leagues/models.py
+++ leagues/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class League(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(default='')
+    members = models.ManyToManyField('friends.Profile', related_name='leagues', blank=True)
 
     def __str__(self):
         return self.name
