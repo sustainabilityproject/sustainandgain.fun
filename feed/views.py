2c155b8dff8924d681d6e6dfb778ef4bb81e344c
Charles MacDonald-Smith <ch@rlie.xyz>
2023-02-26 20:07:00 +0000

    Cleanup

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView
+
 from tasks.models import TaskInstance
-from django.db.models import Q
 
 
 class FeedView(ListView):
@@ -9,5 +9,5 @@ class FeedView(ListView):
     model = TaskInstance
 
     def get_queryset(self):
-        tasks = TaskInstance.objects.exclude(Q(status=TaskInstance.ACTIVE))
+        tasks = TaskInstance.objects.exclude(status=TaskInstance.ACTIVE)
         return tasks
