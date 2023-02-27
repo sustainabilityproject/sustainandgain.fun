b0312d519558c35cf0136e6150bf37dee987df2b
Charles MacDonald-Smith <ch@rlie.xyz>
2023-02-27 19:17:14 +0000

    Update STATIC_ROOT

--- sustainability/settings.py
+++ sustainability/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
