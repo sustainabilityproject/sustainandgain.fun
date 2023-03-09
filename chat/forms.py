9642c7221f46a669862f50b7ad8126e1e3bf855c
James Calnan <james.calnan@outlook.com>
2023-03-09 17:09:20 +0000

    Added chat room input sanitization
    censors words in the word list

--- chat/forms.py
+++ chat/forms.py
@@ -1,9 +1,32 @@
+import re, os
 from django import forms
 from .models import ChatMessage
+from pathlib import Path
+
+CURSE_WORDS = ["fuck", "shit", "bitch"]
+
+def sanitize_input(input_str):
+    path = Path(__file__).resolve().parent.parent / 'static' / 'en.txt'
+    with open(path, 'r') as f:
+        swear_words = [line.strip() for line in f]
+    for word in swear_words:
+        pattern = re.compile(r'\b{}\b'.format(word), re.IGNORECASE)
+        input_str = pattern.sub('*' * len(word), input_str)
+    return input_str
 
 class ChatMessageForm(forms.ModelForm):
     content = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Enter message...', 'class': 'form-control'}))
 
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if content:
+            sanitized_content = sanitize_input(content)
+            if not sanitized_content.isascii():
+                raise forms.ValidationError('Only English characters are allowed.')
+            return sanitized_content
+        return content
+
     class Meta:
         model = ChatMessage
         fields = ['content']
+
