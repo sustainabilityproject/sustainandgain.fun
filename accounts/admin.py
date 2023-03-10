9bfc8529645eabce66795a67bf2bad2ecb77000c
Charles MacDonald-Smith <ch@rlie.xyz>
2023-03-10 13:43:24 +0000

    Add email functionality to authentication

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,1 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BuiltinUserAdmin
 
-from accounts.forms import UserCreationForm, UserChangeForm
-from accounts.models import User
-
-
-class UserAdmin(BuiltinUserAdmin):
-    add_form = UserCreationForm
-    form = UserChangeForm
-    model = User
-    list_display = ["username", "email", ]
-
-
-admin.site.register(User, UserAdmin)
