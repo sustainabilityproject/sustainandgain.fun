1cc75a16f3af863187e2a26f1f61252dcf7cf962
nabe2002 <99815225+nabe2002@users.noreply.github.com>
2023-03-13 11:40:05 +0000

    Added docstrings to friends\forms
    I did so many docstrings in the models and views, how did I forget all of the forms smh

--- friends/forms.py
+++ friends/forms.py
@@ -6,19 +6,43 @@ from .models import Profile
 
 
 class UpdateProfileForm(forms.ModelForm):
+    """
+    Form to update user profile.
+
+    Attributes:
+        first_name (CharField): The first name displayed.
+        last_name (CharField): The last name displayed.
+
+    Methods:
+        __init__(self, *args, **kwargs): TODO
+        save(self, commit): Save changes to the profile.
+    """
     first_name = forms.CharField(max_length=30, required=False)
     last_name = forms.CharField(max_length=30, required=False)
 
     class Meta:
+        """
+        TODO
+        """
         model = Profile
         fields = ['first_name', 'last_name', 'image', 'bio']
 
     def __init__(self, *args, **kwargs):
+        """
+        TODO
+        """
         super().__init__(*args, **kwargs)
         self.fields['first_name'].initial = self.instance.user.first_name
         self.fields['last_name'].initial = self.instance.user.last_name
 
     def save(self, commit=True):
+        """
+        Save changes to the profile.
+        Assign path to image.
+
+        Returns:
+            profile (Profile): The updated profile.
+        """
         profile = super().save(commit=False)
         current_image = Profile.objects.get(user=self.instance.user).image
         if self.cleaned_data['image'] != current_image:
