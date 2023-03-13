ef03e55c2343d2878ce3c1f7f9378783477b2a52
nabe2002 <99815225+nabe2002@users.noreply.github.com>
2023-03-13 12:05:56 +0000

    Added docstrings to leagues\forms
    The docstrings are nearly 100% docstringing

--- leagues/forms.py
+++ leagues/forms.py
@@ -7,6 +7,10 @@ from leagues.models import LeagueMember, League
 class CreateLeagueForm(forms.ModelForm):
     """
     Form used by a user to create a new league.
+
+    Methods:
+        clean_name(self): Two leagues cannot share the same name.
+        clean(self): If the league is private, it must be invite-only.
     """
 
     class Meta:
@@ -14,6 +18,12 @@ class CreateLeagueForm(forms.ModelForm):
         fields = ['name', 'description', 'visibility', 'invite_only']
 
     def clean_name(self):
+        """
+        Two leagues cannot share the same name.
+
+        Returns:
+            name (CharField): The league's name.
+        """
         name = self.cleaned_data['name']
         if League.objects.filter(name=name).exists():
             raise forms.ValidationError('A league with that name already exists')
@@ -22,6 +32,9 @@ class CreateLeagueForm(forms.ModelForm):
     def clean(self):
         """
         If the league is private, it must be invite-only.
+
+        Returns:
+            cleaned_data (dict): The league data.
         """
         cleaned_data = super().clean()
         visibility = cleaned_data.get('visibility')
@@ -34,6 +47,10 @@ class CreateLeagueForm(forms.ModelForm):
 class EditLeagueForm(forms.ModelForm):
     """
     Form used by a league admin to update the league details.
+
+    Methods:
+        clean_name(self): Two leagues cannot share the same name.
+        clean(self): If the league is private, it must be invite-only.
     """
 
     class Meta:
@@ -41,6 +58,12 @@ class EditLeagueForm(forms.ModelForm):
         fields = ['name', 'description', 'visibility', 'invite_only']
 
     def clean_name(self):
+        """
+        Two leagues cannot share the same name.
+
+        Returns:
+            name (CharField): The league's name.
+        """
         name = self.cleaned_data['name']
         if League.objects.filter(name=name).exclude(pk=self.instance.pk).exists():
             raise forms.ValidationError('A league with that name already exists')
@@ -49,6 +72,9 @@ class EditLeagueForm(forms.ModelForm):
     def clean(self):
         """
         If the league is private, it must be invite-only.
+
+        Returns:
+            cleaned_data (dict): The league data.
         """
         cleaned_data = super().clean()
         visibility = cleaned_data.get('visibility')
@@ -61,6 +87,13 @@ class EditLeagueForm(forms.ModelForm):
 class InviteMemberForm(forms.ModelForm):
     """
     Form used by an admin of a league to invite a user to join the league.
+
+    Attributes:
+        username (CharField): The user being invited.
+
+    Methods:
+        clean_username(self): The username entered must belong to an existing user.
+        clean(self): Check if the user is already a member of the league or has already been invited.
     """
     username = forms.CharField(max_length=100)
 
@@ -69,6 +102,12 @@ class InviteMemberForm(forms.ModelForm):
         fields = ['username']
 
     def clean_username(self):
+        """
+        The username entered must belong to an existing user.
+
+        Returns:
+            username (CharField): The username of the invited user.
+        """
         username = self.cleaned_data['username']
         if not User.objects.filter(username=username).exists():
             raise forms.ValidationError('User does not exist')
@@ -77,6 +116,9 @@ class InviteMemberForm(forms.ModelForm):
     def clean(self):
         """
         Check if the user is already a member of the league or has already been invited.
+
+        Returns:
+            cleaned_data (dict): The user's details.
         """
         cleaned_data = super().clean()
         username = cleaned_data.get('username')
