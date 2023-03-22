9fb38daf8f11e726f299315262f3b1c67d5902ca
Charles MacDonald-Smith <ch@rlie.xyz>
2023-03-22 17:49:00 +0000

    Force user back to walkthrough if they go to other page

--- static/js/tour.js
+++ static/js/tour.js
@@ -1,6 +1,18 @@
 if (window.location.search === undefined || window.location.search === '') {
     console.log("hello")
     if (document.cookie.split(';').filter((item) => item.includes('tour=')).length) {
-        window.location.search = '?tour=' + document.cookie.split(';').filter((item) => item.includes('tour='))[0].split('=')[1];
+        let walkthrough = fetch('/static/walkthrough.json').then(response => response.json()).then(
+            (data) => {
+                let currentUrl = window.location.href;
+                let tourCookie = document.cookie.split(';').find((item) => item.includes('tour=')).split('=')[1];
+
+                let tourUrl = data.find((item) => item.tour === tourCookie).url;
+
+                if (!currentUrl.includes(tourUrl)) {
+                    window.location.href = `${tourUrl}?tour=${tourCookie}`;
+                } else {
+                    window.location.href = `?tour=${tourCookie}`;
+                }
+            });
     }
 }
