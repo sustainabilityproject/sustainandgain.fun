56607a092ddb33319eeb25f804a59ef1f16eb299
Charles MacDonald-Smith <ch@rlie.xyz>
2023-03-23 11:21:58 +0000

    Fix notifications for league

--- leagues/signals.py
+++ leagues/signals.py
@@ -24,5 +24,7 @@ def send_league_notification(sender, instance, created, **kwargs):
                         action_object=instance, target=instance.league, url=instance.league.get_absolute_url(), public=False)
 
     if not created and instance.status == 'invited':
-        notify.send(instance.profile, recipient=instance.profile.user, verb='invited you to join their league.',
-                    action_object=instance, target=instance.league, url=instance.league.get_absolute_url(), public=False)
+        if not created and instance.status == 'invited':
+            notify.send(instance.profile, recipient=instance.profile.user, verb='invited you to join their league.',
+                        action_object=instance, target=instance.league, url=instance.league.get_absolute_url(),
+                        public=False)
